0228b0a0968538d5384607f3b095d1bd00155248
ShaojianL <sla335@sfu.ca>
2024-04-06 21:26:21 -0600

    testing flask environment

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 testRepo
 test
 test 2024-04-06
+This is a repo for testing anaconda env setup
+
+export FLASK_PP=app.py
+flask run
---
+++ script_test.py
@@ -0,0 +1,66 @@
+import os
+import tensorflow as tf
+# Load compressed models from tensorflow_hub
+os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
+
+import IPython.display as display
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12, 12)
+mpl.rcParams['axes.grid'] = False
+
+import numpy as np
+import PIL.Image
+import time
+import functools
+
+def tensor_to_image(tensor):
+  tensor = tensor*255
+  tensor = np.array(tensor, dtype=np.uint8)
+  if np.ndim(tensor)>3:
+    assert tensor.shape[0] == 1
+    tensor = tensor[0]
+  return PIL.Image.fromarray(tensor)
+
+content_path = tf.keras.utils.get_file('FBprofilephoto2.jpg', 'https://scontent.fyyc7-1.fna.fbcdn.net/v/t39.30808-6/260249229_10158169686732477_821383534035811114_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=JOTobhdviRoAb4YPpR5&_nc_ht=scontent.fyyc7-1.fna&oh=00_AfCvX10eJdmsP0DBFdhnu_MSsqyXoAAMfU3Kbo1ECUENRw&oe=6617D287')
+style_path = tf.keras.utils.get_file('StarryNight.jpg','https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/1920px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg')
+
+def load_img(path_to_img):
+  max_dim = 512
+  img = tf.io.read_file(path_to_img)
+  img = tf.image.decode_image(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+
+  new_shape = tf.cast(shape * scale, tf.int32)
+
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
+
+def imshow(image, title=None):
+  if len(image.shape) > 3:
+    image = tf.squeeze(image, axis=0)
+
+  # plt.imshow(image)
+  # if title:
+  #   plt.title(title)
+
+content_image = load_img(content_path)
+style_image = load_img(style_path)
+
+plt.subplot(1, 2, 1)
+imshow(content_image, 'Content Image')
+
+plt.subplot(1, 2, 2)
+imshow(style_image, 'Style Image')
+
+import tensorflow_hub as hub
+file_name = 'stylized-image.png'
+hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+stylized_image = hub_model(tf.constant(content_image), tf.constant(style_image))[0]
+tensor_to_image(stylized_image).save(file_name)
